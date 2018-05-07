2d1d717336d2eb24d6dd37fb9beba6f31b9ab386
IuliaRobas <iulia.robas@yahoo.com>
2018-05-07 12:52:16 +0300

    created PacientSettingsPage

---
+++ src/pages/Pacients/PacientSettingsPage.react.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+import LibraryLoader from 'utils/LibraryLoader';
+import AjaxUtils from 'utils/AjaxUtils.js';
+
+class PacientSettingsPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            
+        }
+    }
+    
+    componentDidMount() {
+        const self = this;
+
+        // Initial Ajax requests
+    }
+    
+    render() {
+        return (
+            <div id="mainCnt">
+                <div id="title">TITLE</div>
+                { /* page come here */ }
+            </div>
+        )
+    }
+};
+
+export default withRouter(PacientSettingsPage);
